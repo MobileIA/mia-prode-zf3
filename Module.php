9bee4a438eaefd8699516cfaa56696442ca34cc7
Matias Camiletti <matias.camiletti@gmail.com>
2018-02-01 19:14:03 -0300

    Estructura inicial del modulo.

---
+++ Module.php
@@ -0,0 +1,21 @@
+<?php
+namespace MIAProde;
+
+class Module implements \Zend\ModuleManager\Feature\ConfigProviderInterface
+{
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+}
