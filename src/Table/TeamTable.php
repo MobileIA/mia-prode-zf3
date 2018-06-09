5a32a2ec63b052300516696e0a6fcea98178b89f
Matias Camiletti <matias.camiletti@gmail.com>
2018-06-09 11:29:41 -0300

    Agregada funciones en la tabla de equipos.

--- src/Table/TeamTable.php
+++ src/Table/TeamTable.php
@@ -16,4 +16,23 @@ class TeamTable extends \MIABase\Table\Base
     {
         return $this->tableGateway->select(array('title' => $title))->current();
     }
+    /**
+     * Obtiene equipo por externalID
+     * @param int $externalId
+     * @return \Application\Entity\Team|null
+     */
+    public function fetchByExternalId($externalId)
+    {
+        return $this->tableGateway->select(array('external_id' => $externalId))->current();
+    }
+    /**
+     * Obtiene equipo por titulo y pais
+     * @param string $title
+     * @param int $countryId
+     * @return \Application\Entity\Team|null
+     */
+    public function fetchByTitleAndCountry($title, $countryId)
+    {
+        return $this->tableGateway->select(array('title' => $title, 'country_id' => $countryId))->current();
+    }
 }
